78e7c7b9c40faff923bfaf7028f48d8f3e7b5e4c
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-05-05 21:35:24 +0000

    the trafic light was created

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [light, setLight] = useState("");
+  return (
+    <>
+      <div className="traffic-light-container">
+        <div className="lamp-post"></div>
+        <div className="lamp-box">
+          <div className={"light red" + ((light === "red") ?" on-red": "")} onClick={()=>setLight("red")}></div>
+          <div className={"light yellow" + ((light === "yellow") ?" on-yellow": "") } onClick={()=>setLight("yellow")}></div>
+          <div className={"light green" + ((light === "green") ?" on-green": "") } onClick={()=>setLight("green")}></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
